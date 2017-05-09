6eb0352345e364a43424c1254e122c819d57b021
Ryan McAlpin <rhmcalpin@gmail.com>
2017-05-09 16:46:59 -0700

    add firebase coursework

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent } from './about/about.component';
 import { MarketplaceComponent }   from './marketplace/marketplace.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { AdminComponent } from './admin/admin.component';
 
 const appRoutes: Routes = [
   {
@@ -16,6 +18,14 @@ const appRoutes: Routes = [
   {
     path: 'marketplace',
     component: MarketplaceComponent
+  },
+  {
+    path: 'albums/:id',
+    component: AlbumDetailComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
   }
 ];
 
--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Album } from '../album.model';
+import { AlbumService } from '../album.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService]
 })
+
 export class MarketplaceComponent implements OnInit {
+  albums: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private albumService: AlbumService) {}
 
-  ngOnInit() {
+  ngOnInit(){
+    this.albums = this.albumService.getAlbums();
   }
 
+  goToDetailPage(clickedAlbum) {
+    this.router.navigate(['albums', clickedAlbum.$key]);
+  }
 }
